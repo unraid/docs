f80de09909796bf89606604a96182ca4ccd2cc94
Eli Bosley <ekbosley@gmail.com>
2023-05-19 16:55:33 -0400

    feat: heading level set 4 max

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -106,6 +106,10 @@ const config = {
           },
         ],
       },
+      tableOfContents: {
+        minHeadingLevel: 2,
+        maxHeadingLevel: 4,
+      },
       footer: {
         style: "dark",
         links: [
