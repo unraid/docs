81fe508292d84a6b5a7c9280589773a5fe24facf
Eli Bosley <ekbosley@gmail.com>
2023-05-22 10:20:15 -0400

    feat: ga-4 in docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,6 +57,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        gtag: {
+          trackingID: 'G-CZENQ1ZPEH',
+          anonymizeIP: true
+        }
       }),
     ],
   ],
