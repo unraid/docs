7d4e3032dc68ee2ffe887ddec673dcb71f363504
Eli Bosley <ekbosley@gmail.com>
2023-05-24 12:25:02 -0400

    fix: edit localized files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,6 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/unraid/docs/tree/main/",
+          editLocalizedFiles: true,
           async sidebarItemsGenerator({ defaultSidebarItemsGenerator, ...args}) {
             const sidebarItems = await defaultSidebarItemsGenerator(args);
             return sortSidebarItems(sidebarItems);
