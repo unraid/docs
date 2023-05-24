1504cf2103e83c494a98194ba65b57391d0f6201
ljm42 <larry_meaney@iname.com>
2023-05-24 10:54:34 -0700

    When building, warn on broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,8 +23,8 @@ const config = {
   organizationName: "unraid", // Usually your GitHub org/user name.
   projectName: "docs", // Usually your repo name.
 
-  onBrokenLinks: "log",
-  onBrokenMarkdownLinks: "log",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
