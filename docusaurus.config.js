734614bb71d67ab2135018f1bdac036b1ef2fe4d
Paul Taylor <pauldavidtaylor@gmail.com>
2023-09-22 00:00:12 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -250,7 +250,7 @@ const config = {
           },
           {
             to: "/unraid-os/overview/",
-            from: " /unraid-os/overview/what-is-unraid/",
+            from: "/unraid-os/overview/what-is-unraid/",
           },
         ],
       },
