b7605e04f85b7b8093e16b429c222df00b365a99
ljm42 <larry_meaney@iname.com>
2025-05-23 16:20:05 -0700

    Remove details on submitting PRs
    We are in the process of reorganizing / rewriting the docs, disable contributions for now

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,8 +50,8 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/unraid/docs/tree/main/",
-          editLocalizedFiles: true,
+          // editUrl: "https://github.com/unraid/docs/tree/main/",
+          // editLocalizedFiles: true,
           async sidebarItemsGenerator({
             defaultSidebarItemsGenerator,
             ...args
