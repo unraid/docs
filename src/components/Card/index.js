32b7c064957eec89d8a5425de85f93789328a991
dopeytree <dopeytree@gmail.com>
2025-10-18 19:38:46 +0100

    Update Developer documentation structure and add new content

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,38 @@
 import React, { CSSProperties } from 'react'; // CSSProperties allows inline styling with better type checking.
 import clsx from 'clsx'; // clsx helps manage conditional className names in a clean and concise manner.
+import Link from '@docusaurus/Link';
+
 const Card = ({
   className, // Custom classes for the container card
   style, // Custom styles for the container card
   children, // Content to be included within the card
   shadow, // Used to add shadow under your card. Expected values are: low (lw), medium (md), tall (tl)
+  href, // Link URL for the card
+  title, // Title for the card header
+  icon, // Icon for the card header
 }) => {
   const cardShadow = shadow ? `item shadow--${shadow}` : '';
-  return (
+  const cardContent = (
     <div className={clsx('card', className, cardShadow)} style={style}>
+      {(title || icon) && (
+        <div className="card__header">
+          {icon && <span className={`icon icon-${icon}`}></span>}
+          {title}
+        </div>
+      )}
       {children}
     </div>
   );
+
+  if (href) {
+    return (
+      <Link to={href} className="card-link">
+        {cardContent}
+      </Link>
+    );
+  }
+
+  return cardContent;
 };
+
 export default Card;
