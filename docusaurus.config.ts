c10627eb5c38ad4f6289d3b9656c4cd970bd23ef
Eli Bosley <11823237+elibosley@users.noreply.github.com>
2026-05-25 05:39:48 -0400

    New Crowdin updates

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -68,7 +68,11 @@ const config: Config = {
   projectName: "docs", // Usually your repo name.
 
   onBrokenLinks: "warn",
-  onBrokenMarkdownLinks: "warn",
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+    },
+  },
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
